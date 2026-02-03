bb618fba5c005bb0c4dd3aa31f0addbf9a591aee
jkulmus <jacquelynkulmus@gmail.com>
2026-02-02 16:53:29 -0800

    added X to remove items from cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,18 +1,28 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  const removeButtons = document.querySelectorAll(".cart-card__remove");
+  removeButtons.forEach(button => {
+    button.addEventListener("click", () => removeFromCart(button.dataset.id));
+  });
+}
+
+function removeFromCart(id) {
+  const cartItems = getLocalStorage("so-cart") || [];
+  const updatedCart = cartItems.filter((item) => item.Id !== id);
+  setLocalStorage("so-cart", updatedCart);
+  renderCartContents();
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <span class="cart-card__remove" data-id="${item.Id}">X</span>
   <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
+    <img src="${item.Image}" alt="${item.Name}" />
   </a>
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
