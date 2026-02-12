a44b52514c19caf12ec12094b14a393743c81115
Brynlee Bingham <[Brynbing03]@users.noreply.github.com>
2026-02-12 12:59:04 -0700

    pushing everything again

---
+++ src/js/product-list.js
@@ -0,0 +1,19 @@
+import productList from "./productList.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+const category = getParam("category");
+productList(".product-list", category);
+
+
+
+
+const label = category
+  .split("-")
+  .map(word => word[0].toUpperCase() + word.slice(1))
+  .join(" ");
+
+document.querySelector("h1").textContent = `Top Products: ${label}`;
+
+productList(".product-list", catagory);
+
