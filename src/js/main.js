3f8dfe5153462354291c7e8f7a5b7fac24fb0ff1
jamfrancis <jamestelfordfrancis@gmail.com>
2026-02-12 12:39:15 -0700

    Team activity move to API from JSON

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import productList from "./productList.mjs";
+
 import { loadHeaderFooter } from "./utils.mjs";
 import loadAlerts from "./alert.js";
 
 loadHeaderFooter();
 loadAlerts();
 
-productList(".product-list", "tents");
