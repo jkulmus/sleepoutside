507a33d04135f679e9d1bd579429d25405bea3fc
jkulmus <jacquelynkulmus@gmail.com>
2026-02-26 17:33:01 -0800

    team 7

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -17,3 +17,16 @@ document.forms["checkout"].addEventListener("submit", (e) => {
   
   checkoutProcess.checkout(e.target);
 });
+
+document.querySelector("#checkoutSubmit")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+    const myForm = document.forms[0];
+    const chk_status = myForm.checkValidity();
+
+    myForm.reportValidity();
+    if (chk_status) {
+      checkoutProcess.checkout();
+    } 
+  })
+  
