62b60ea77225b40e47a531276d7ab18aad9a736f
klee2187 <kkelly6@byui.edu>
2026-03-06 10:53:34 -0700

    authentication team activity

---
+++ src/js/login.js
@@ -0,0 +1,13 @@
+import { loadHeaderFooter, getParam } from "../utils.mjs";
+import { login } from "../auth.js";
+
+loadHeaderFooter();
+
+const redirect = getParam(redirect);
+
+document.querySelector("#login-button").addEventListener("click", (e) => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    login({ email, password }, redirect);
+});
